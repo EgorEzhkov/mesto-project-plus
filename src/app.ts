552ebcc45a4e314ff598835132f49967a4c2d901
Egor Ezhkov <egorkashpak21@yandex.ru>
2024-03-17 20:09:08 +0300

    Подготовка к работе

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import path from "path";
+import express from "express";
+import mongoose from "mongoose";
+
+const { PORT = 3000 } = process.env;
+const app = express();
+
+mongoose.connect("mongodb://localhost:27017/mestodb");
+
+app.listen(PORT, () => {
+  console.log(`Порт сервера: ${PORT} `);
+});
